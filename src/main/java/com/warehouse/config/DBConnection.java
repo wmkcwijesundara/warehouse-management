d515a493d43dca0575fd8434263e9590aa8e4c6f
Kavindu Chelaka <chelaka560@gmail.com>
2026-01-04 18:57:56 +0530

    Update database connection details in DBConnection.java

--- src/main/java/com/warehouse/config/DBConnection.java
+++ src/main/java/com/warehouse/config/DBConnection.java
@@ -2,9 +2,9 @@ package com.warehouse.config;
 import java.sql.*;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/arinest";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/arinest?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC"";
+    private static final String USER = "warehouse_user";
+    private static final String PASSWORD = "jens123";
 
     public static Connection getConnection() throws SQLException {
         try {
